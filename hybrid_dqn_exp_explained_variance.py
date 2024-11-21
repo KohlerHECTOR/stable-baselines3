1ca335ad4ac390bcfcad6f8f122f0f7dfe9a766c
KohlerHECTOR <hkohler@fnancy>
2024-11-21 16:48:27 +0100

    added fix

--- hybrid_dqn_exp_explained_variance.py
+++ hybrid_dqn_exp_explained_variance.py
@@ -13,17 +13,14 @@ params_cartpole_zoo = {
     "gradient_steps": 128,
     "exploration_fraction": 0.16,
     "exploration_final_eps": 0.04,
-    "policy_kwargs": dict(net_arch=[256, 256]),
-    "new_estimators":1,
-    "lr_gb":2.3e-3,
-    "depth":3,
+    "policy_kwargs": dict(net_arch=[256, 256], new_estimators=1, lr_gb=2.3e-3, depth=3),
     "gb_freq":1,
 }
 
 
 for seed in range(10):
     for new_estimator in [1, 10, 100]:
-        params_cartpole_zoo["new_estimators"] = new_estimator
+        params_cartpole_zoo["policy_kwargs"]["new_estimators"] = new_estimator
         env = gym.make("CartPole-v1")
         model = HybridDQN("HybridPolicy", env, seed=seed, **params_cartpole_zoo, tensorboard_log="./hybrid_dqn/")
         model.learn(1e5, tb_log_name=f"new_estimators_{new_estimator}_seed_{seed}")
